9596f706edcc9b8ca6452c12d8dcb3e7b1ac33bf
japhet Omorodion <japhet.omorodion@gmail.com>
2019-02-08 10:10:18 +0100

    settings and apps admin.py altered

--- Real_Estate/settings.py
+++ Real_Estate/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Real_Estate/static/'),
 ]
+
+# Media files handling
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
